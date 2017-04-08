0e54d985cc3b5af642379de87b575234035034b8
0x00000FF <doctor@kangnang.net>
2017-04-08 23:28:36 +0900

    This is the main routine of rensenWare
    except the encryption/decryption processes

--- frmWarning.cs
+++ frmWarning.cs
@@ -1,12 +1,9 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
 using System.Drawing;
 using System.Diagnostics;
-using System.Linq;
 using System.Runtime.InteropServices;
-using System.Text;
 using System.Threading;
 using System.Windows.Forms;
 using System.Security.Cryptography;
